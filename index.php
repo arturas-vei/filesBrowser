468bbbdd098a8b1f6b8cef438f722510eb5d54e9
arturas-vei <74532989+arturas-vei@users.noreply.github.com>
2021-03-04 10:34:29 +0200

    structure

--- index.php
+++ index.php
@@ -50,8 +50,63 @@
     print('<table>');
     print('<tr><th>File or Folder</th><th>Name</th><th>Action</th></tr>');
 
-    
+    // main structure - scan current directory and show files/folders
+    foreach ($files as $file) {
+        print('<tr>');
+        if ($file != '.' && $file != '..') {
+            print('<td>'. (is_dir($path . $file) ? '<img src="img/folder_pic.png">' : '<img src="img/file_pic.png">') .'</td>');
+            print('<td>');
+            if (is_dir($path . $file) == 'true') {
+                print('<a href="');
+                if (isset($_GET['my_way']) == 'true') {
+                    print($_SERVER['REQUEST_URI'] . $file . '/'. '">' . $file . '</a>');
+                } else print($_SERVER['REQUEST_URI'] . '?my_way=/' . $file . '/'. '">' . $file . '</a>');
+            } else 
+            print($file.'</td>'); 
+            //delete/download file buttons
+            print((is_file($path . $file) ? '<td>
+                <form method="post" style="display: inline-block"><input type="hidden" name="deletion" value='.str_replace(' ', '&nbsp;', $file).'><input type="submit" value="Delete"></form>
+                <form method="post" style="display: inline-block"><input type="hidden" name="download" value='.str_replace(' ', '&nbsp;', $file).'><input type="submit" value="Download"></form>
+                </td>' 
+                : "<td> Don't touch :) </td>"));
+        }       
+        print('</tr>');
+    }
+    print('</table>');
+    echo '<br>';
+
+    //back button
+    print("\t".'<button><a href="');
+    $back_fake = explode('/', $_SERVER['QUERY_STRING']);
+    $back_real = explode('/', $_SERVER['QUERY_STRING'],-2);
+    if (count($back_fake) == 1 || count($back_fake) == 2) {
+        print('?my_way=/'.'">BACK button</a>');
+    } else
+        print('?'.implode('/',$back_real).'/'.'">BACK button</a>');
+        print('</button><br>')
     ?>
-    
+
+    <!-- upload file -->
+    <br>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload"/>
+        <button type="submit">Upload file</button>
+    </form>
+
+    <!-- new directory -->
+    <br>
+    <form method="get"> 
+        <input type="hidden" name="my_way" value="<?php print($_GET['my_way']) ?>" /> 
+        <input placeholder="New folder name" type="text" name="new_dir">
+        <button type="submit">Create</button>
+    </form>
+
+    <!-- logout button -->
+    <br>
+     <form action="index.php" method="post"> 
+        <input type="hidden" name="logout">
+        <button type="submit">Logout!</button>
+    </form>
+
 </body>
 </html>
